05cd5b90ed1796611b15080f5c9c5f746cb8bce5
mubashir369 <muhammedmubashir3693@gmail.com>
2025-10-09 12:48:40 +0530

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mubz App",
+  title: "ELIAABYTE App",
   description: "a",
 };
 
